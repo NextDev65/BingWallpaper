0e7c36f68f2be048f051e93f1adb459da41cf933
Matterhorn <12612637+Matterhorn56@users.noreply.github.com>
2020-06-10 16:59:16 -0500

    changed image title selector
    previously image title element had id of "iotd_title", now selecting by second element with class "title"

--- BingWallpaper.js
+++ BingWallpaper.js
@@ -1,7 +1,7 @@
-document.getElementsByClassName('shaders')[0].remove();
-document.getElementsByClassName('hp_cont')[0].remove();
-document.getElementById('scroll_cont').className = 'scroll_cont show_none';
-document.getElementsByClassName('vs')[0].remove();
+document.getElementsByClassName("shaders")[0].remove();
+document.getElementsByClassName("hp_cont")[0].remove();
+document.getElementById("scroll_cont").className="scroll_cont show_none";
+document.getElementsByClassName("vs")[0].remove();
 
-document.getElementsByClassName('musCard')[0].style.opacity=.56;
-alert(document.getElementById('iotd_title').innerHTML + '.jpg');
+document.getElementsByClassName("musCard")[0].style.opacity=.56;
+alert(document.getElementsByClassName("title")[1].innerHTML+".jpg");
