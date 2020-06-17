007bece441fcc3f541370b3dd5fc9fda1bad5017
Matterhorn <12612637+Matterhorn56@users.noreply.github.com>
2020-06-17 13:29:40 -0500

    Update BingWallpaper.js
    Works on both bing.com and bing New Tab

--- BingWallpaper.js
+++ BingWallpaper.js
@@ -4,4 +4,14 @@ document.getElementById("scroll_cont").className="scroll_cont show_none";
 document.getElementsByClassName("vs")[0].remove();
 
 document.getElementsByClassName("musCard")[0].style.opacity=.56;
-alert(document.getElementsByClassName("title")[1].innerHTML+".jpg");
+if (document.getElementsByClassName("title")[1].innerHTML) {
+  name = document.getElementsByClassName("title")[1].innerHTML;
+}
+else {
+  name = document.getElementById("iotd_title").innerHTML;
+}
+alert(name+".jpg");
+
+
+if (name == "") {name = document.getElementById("iotd_title").innerHTML;}
+alert(document.getElementById("iotd_title").innerHTML);
