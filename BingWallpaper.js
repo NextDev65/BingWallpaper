f1e4cb0fa7b1842ffd1ae377e5ab7954addccad1
Matterhorn <12612637+Matterhorn56@users.noreply.github.com>
2020-07-01 12:58:07 -0500

    Yay! Now you can run it again without refreshing!
    If you missed a few days you can get them one by one without having to refresh each time.

--- BingWallpaper.js
+++ BingWallpaper.js
@@ -1,17 +1,13 @@
-document.getElementsByClassName("shaders")[0].remove();
-document.getElementsByClassName("hp_cont")[0].remove();
-document.getElementById("scroll_cont").className="scroll_cont show_none";
-document.getElementsByClassName("vs")[0].remove();
-
-document.getElementsByClassName("musCard")[0].style.opacity=.56;
-if (document.getElementsByClassName("title")[1].innerHTML) {
+try {
+  document.getElementsByClassName("shaders")[0].remove();
+  document.getElementsByClassName("hp_cont")[0].remove();
+  document.getElementById("scroll_cont").className="scroll_cont show_none";
+  document.getElementsByClassName("vs")[0].remove();
+  
+  document.getElementsByClassName("musCard")[0].style.opacity=.56;
   name = document.getElementsByClassName("title")[1].innerHTML;
+  if (name == "") {name = document.getElementById("iotd_title").innerHTML;}
 }
-else {
-  name = document.getElementById("iotd_title").innerHTML;
+finally {
+  alert(document.getElementById("iotd_title").innerHTML);
 }
-alert(name+".jpg");
-
-
-if (name == "") {name = document.getElementById("iotd_title").innerHTML;}
-alert(document.getElementById("iotd_title").innerHTML);
