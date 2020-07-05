dbe31437add1c6345c023408e79b6d99384e170a
Matterhorn <12612637+Matterhorn56@users.noreply.github.com>
2020-07-05 13:12:43 -0500

    Fixed bug
    Previous commit broke the title alert on bing new tab

--- BingWallpaper.js
+++ BingWallpaper.js
@@ -5,9 +5,9 @@ try {
   document.getElementsByClassName("vs")[0].remove();
   
   document.getElementsByClassName("musCard")[0].style.opacity=.56;
-  name = document.getElementsByClassName("title")[1].innerHTML;
-  if (name == "") {name = document.getElementById("iotd_title").innerHTML;}
+  name = document.getElementById("iotd_title").innerHTML;
+  if (!name) {name = document.getElementsByClassName("title")[1].innerHTML;}
 }
 finally {
-  alert(document.getElementById("iotd_title").innerHTML);
+  alert(name + ".jpg");
 }
