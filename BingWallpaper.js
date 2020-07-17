58dbe2dbf55a94a06edf63c6eef0ace29184beaa
Matterhorn <12612637+Matterhorn56@users.noreply.github.com>
2020-07-17 11:38:23 -0500

    Update BingWallpaper.js

--- BingWallpaper.js
+++ BingWallpaper.js
@@ -1,10 +1,10 @@
 try {
   document.getElementsByClassName("shaders")[0].remove();
   document.getElementsByClassName("hp_cont")[0].remove();
-  document.getElementById("scroll_cont").className="scroll_cont show_none";
+  document.getElementById("scroll_cont").className = "scroll_cont show_none";
   document.getElementsByClassName("vs")[0].remove();
   
-  document.getElementsByClassName("musCard")[0].style.opacity=.56;
+  document.getElementsByClassName("musCard")[0].style.opacity = .56;
 }
 finally {
   name = document.getElementsByClassName("title")[1].innerHTML;
