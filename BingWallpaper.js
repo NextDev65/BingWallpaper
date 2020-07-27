97635d7a155fb73d29afbf345ada883e821695d0
Matterhorn <12612637+Matterhorn56@users.noreply.github.com>
2020-07-27 12:53:02 -0500

    var

--- BingWallpaper.js
+++ BingWallpaper.js
@@ -7,10 +7,10 @@ try {
   document.getElementsByClassName("musCard")[0].style.opacity = .56;
 }
 finally {
-  name = document.getElementsByClassName("title")[1].innerHTML;
+  var name = document.getElementsByClassName("title")[1].innerHTML;
   if (!name) {name = document.getElementById("iotd_title").innerHTML;}
   
-  dlButton = document.getElementsByClassName("downloadLink")[0];
+  var dlButton = document.getElementsByClassName("downloadLink")[0];
   dlButton.download = name + ".jpg";
   dlButton.click();
 }
