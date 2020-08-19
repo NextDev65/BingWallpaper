7e9329aca3ca5a6cb2e73b286327399a190422c4
Matterhorn <12612637+Matterhorn56@users.noreply.github.com>
2020-08-18 19:38:52 -0500

    Updated Chrome Extension

--- Chrome Extension/content.js
+++ Chrome Extension/content.js
@@ -1,4 +1,4 @@
-for (i = 1; i < 8; i++) {
+for (i = 1; i < 21; i++) {
     setTimeout(function () {
         try {
             document.getElementsByClassName("shaders")[0].remove();
@@ -11,7 +11,11 @@ for (i = 1; i < 8; i++) {
         if (!title) {
             title = document.getElementById("iotd_title").innerHTML;
         }
-
+		
+		// document.getElementsByClassName("downloadLink")[0].href.split("&rf")[0]
+		var dlButton = document.getElementsByClassName("downloadLink")[0];
+        dlButton.download = title + ".jpg";
+		
         // by passing an object you can define default values e.g.: []
         chrome.storage.sync.get({
             sevenNames: []
@@ -19,21 +23,20 @@ for (i = 1; i < 8; i++) {
             // the input argument is ALWAYS an object containing the queried keys
             // so we select the key we need
             sevenDays = result.sevenNames;
+            //console.log(sevenNames);
+            console.log(sevenDays);
             if (!sevenDays.includes(title)) {
                 sevenDays.unshift(title);
-                var dlButton = document.getElementsByClassName("downloadLink")[0];
-                dlButton.download = title + ".jpg";
                 setTimeout(function () {
                     dlButton.click();
                 }, 1000);
             }
-            if (sevenDays.length > 7) {
+            if (sevenDays.length > 0) {
                 sevenDays.pop();
             }
             // set the new array value to the same key
             chrome.storage.sync.set({
-                sevenNames: sevenDays,
-                HasBeenUploadedYet: false
+                sevenNames: sevenDays
             });
         });
 
