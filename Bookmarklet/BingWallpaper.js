ef372e387d81d5c0aaae3434751758e1595badb9
NextDev65 <12612637+NextDev65@users.noreply.github.com>
2023-03-05 17:30:15 -0600

    Update BingWallpaper.js
    remove floating quiz
    formatting

--- Bookmarklet/BingWallpaper.js
+++ Bookmarklet/BingWallpaper.js
@@ -1,16 +1,21 @@
 try {
-  document.getElementsByClassName("shaders")[0].remove();
-  document.getElementsByClassName("hp_cont")[0].remove();
-  document.getElementById("scroll_cont").className = "scroll_cont show_none";
-  document.getElementsByClassName("vs")[0].remove();
-  
-  document.getElementsByClassName("musCard")[0].style.opacity = .56;
+    // top shader
+    document.getElementsByClassName("shaders")[0].remove();
+    // top bar
+    document.getElementsByClassName("hp_cont")[0].remove();
+    // floating quiz
+    document.getElementsByClassName('hp_trivia_outer show')[0].remove();
+    // bottom carousel
+    document.getElementById("scroll_cont").className = "scroll_cont show_none";
+    document.getElementsByClassName("vs")[0].remove();
+    // description card
+    document.getElementsByClassName("musCard")[0].style.opacity = .50;
 }
 finally {
-  var name = document.getElementsByClassName("title")[1].innerHTML;
-  if (!name) {name = document.getElementById("iotd_title").innerHTML;}
+    var name = document.getElementsByClassName("title")[1].innerHTML;
+    if (!name) {name = document.getElementById("iotd_title").innerHTML;}
   
-  var dlButton = document.getElementsByClassName("downloadLink")[0];
-  dlButton.download = name + ".jpg";
-  dlButton.click();
+    var dlButton = document.getElementsByClassName("downloadLink")[0];
+    dlButton.download = name + ".jpg";
+    dlButton.click();
 }
